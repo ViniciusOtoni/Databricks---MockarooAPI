d81ee7f9f5fec898636c2db6fbf692229786738e
ViniciusOtoni <viniciusotonimsv@gmail.com>
2024-08-25 15:29:53 -0300

    utilizando a API do Mockaroo para gerar valores aleatórios

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -31,17 +31,16 @@ def create_JSON_files(data):
     output_dir = '../../data/raw'
     os.makedirs(output_dir, exist_ok=True)
 
-    articles = data['articles']
-
-    df = pd.DataFrame(articles)
+    
+    df = pd.DataFrame(data)
 
     for index, row in df.iterrows(): #itera cada linha do df.
         
         row_dict = row.to_dict() #criando o dicionário para o json transformando cada linha em dicionário
 
-        published_date = datetime.datetime.strptime(row_dict['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
+        release_date = datetime.datetime.strptime(row_dict['release_date'], '%Y-%m-%dT%H:%M:%S%z')
+        file_name = f"{release_date.strftime('%d-%m-%Y_%H-%M-%S')}.json" # Gerando nome do arquivo JSON
 
-        file_name = f"{published_date.strftime('%d-%m-%Y_%H-%M-%S')}.json" #gerando nome do arquivo JSON
         file_path = os.path.join(output_dir, file_name) #enviando os dados para o diretório
 
         with open(file_path, 'w') as json_file:
--- src/raw/main.py
+++ src/raw/main.py
@@ -1,41 +1,45 @@
 import sys
 import os
+import json
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src/lib')))
 import utils
 import connection
+import mockaroo
 
-import requests
 import dotenv
 
-
 dotenv.load_dotenv(".env") #variaveis de ambiente
-newsapi_key = os.getenv("NEWSAPI_KEY") #chave de acesso da API
 
+mockaroo_key = os.getenv("MOCKAROO_API_KEY") #chave de acesso da API
+
+
+with open('fields.json', 'r') as file:
+    fields = json.load(file)
 
 params = {
-    'q': 'economy', #target
-    'language': 'en',
-    'pageSize': 100, 
-    'apiKey': newsapi_key
+    "key": mockaroo_key,
+    "count": 100
 }
 
-response = requests.get("https://newsapi.org/v2/everything", params=params)
-
-if response.status_code == 200:
-    data = response.json()
-    utils.create_JSON_files(data)
-else:
-    print(f"erro na requisição: {response.status_code}")
-
-
-
-path_file = '../../data/raw/'
-file_type = 'json'
-
 def main():
+
+    api_url = "https://api.mockaroo.com/api/generate.json"
+
+    mock = mockaroo.Mockaroo(api_url, fields, params)
+    data = mock.generateData()
+
+    if data:
+        utils.create_JSON_files(data)
+    else:
+        return
+
+    path_file = '../../data/raw/'
+    file_type = 'json'
+
     azure_connected = connection.ConnectionBlobStorage('raw', path_file)
     azure_connected.sendFiles(file_type)
+
 
 if __name__ == "__main__":
     main()
